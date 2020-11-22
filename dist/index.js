cca6834c6a49080e9a7502a7a1d31d5a98fc29d4
afeiship <1290657123@qq.com>
2020-11-22 14:52:34 +0800

    Release 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
  * description: Md5 for next based on blueimp-md5.
  * homepage: https://github.com/afeiship/next-md5
  * version: 1.0.0
- * date: 2020-11-22 14:50:54
+ * date: 2020-11-22 14:52:29
  * license: MIT
  */
 
